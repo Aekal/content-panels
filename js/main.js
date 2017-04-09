b21db34a7b449938eac8f96936178ff7b4568548
Damian Fiałkiewicz <aekal4@gmail.com>
2017-04-09 15:12:10 +0200

    Add tab panel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(function() {
+	//Cache DOM
+	var $tabList = $(".tab-list");
+
+	//Accordion list
 	$(".accordion-list").on("click", "button", function() {
 		$(this).next().slideToggle();
+	});
+
+	//Tab Panel
+	$tabList.on("click", "a", function(e) {
+		e.preventDefault();
+		var $oldTab = $tabList.find("a.active");
+		var $newTab = $(this);
+		var $oldContent = $(".tab-content.active");
+		var link = $newTab.attr("href");
+
+		$oldTab.removeClass("active");
+		$newTab.addClass("active")
+		$oldContent.removeClass("active");
+		$(link).addClass("active");
 	});
 });
