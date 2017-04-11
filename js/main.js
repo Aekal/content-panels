01ce351eb1dde47351088d33af4e7b1957e245cb
Damian Fiałkiewicz <aekal4@gmail.com>
2017-04-11 20:49:07 +0200

    Add modal window

--- js/main.js
+++ js/main.js
@@ -20,4 +20,44 @@ $(function() {
 		$oldContent.removeClass("active");
 		$(link).addClass("active");
 	});
+
+	//Open modal window
+	$("#modal-btn").on("click", function() {
+		modal.open($("<img src='img/work7.png'>"));
+	})
 });
+
+//Modal window - revealing module pattern
+var modal = (function() {
+	//Create DOM components
+	var $modalBg = $("<div class='modal-bg'/>")
+	var $modal = $("<div class='modal'/>");
+	var $content = $("<div class='modal-content'/>");
+	var $closeBtn = $("<button class='modal-close'>Zamknij</button>");
+	$modal.append($content, $closeBtn).appendTo($modalBg);
+	//Close modal on button click
+	$closeBtn.on("click", function () {
+		modal.close();
+	});
+	//Close modal on click outside the container
+	$(window).on("click", function(e) {
+		if ($(e.target).is($modalBg)) {
+			modal.close();
+		}
+	});
+
+	//Public functions
+	return {
+		open: function(content, w, h) {
+			$content.empty().append(content);
+			$modal.css({
+				width: w || 'auto',
+				height: h || 'auto'
+			});
+			$(".modal-container").append($modalBg);
+		},
+		close: function() {
+			$modalBg.detach();
+		}
+	}
+}());
